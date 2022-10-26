79dd763732b2c8762729e20c441253f4a2dfd306
Shahab Saalami <Shab.salami@gmail.com>
2022-10-26 19:50:40 +0330

    clean code

--- app/src/main/java/com/example/myapplication/RoundCabin.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication
-
-open class RoundCabin(resident: Int) : Dwelling(resident) {
-    override val buildingMaterial: String ="Ney"
-    override val capacity: Int = 4
-}
--- app/src/main/java/com/example/myapplication/RoundTower.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication
-
-class RoundTower(residents: Int, val floors: Int) : RoundCabin(residents) {
-    override val buildingMaterial = "Stone"
-    override val capacity = 4 * floors
-}
