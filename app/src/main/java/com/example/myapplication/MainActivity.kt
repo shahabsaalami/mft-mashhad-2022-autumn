3fb087bee4cdc5127e44efdda9686128cffdc4b4
Shahab Saalami <Shab.salami@gmail.com>
2022-10-26 20:15:21 +0330

    create random number

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlin.math.floor
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val slices = 6
+        val random = Random()
+        val randomNumber = random.nextInt(slices) + 1
+        Log.w("MainActivity", " Random Number = ${randomNumber}")
 
 
     }
