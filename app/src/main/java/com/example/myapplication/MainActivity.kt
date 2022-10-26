7059efdb77068cc3ff3b235d459c5c45cb637d5e
Shahab Saalami <Shab.salami@gmail.com>
2022-10-26 22:20:56 +0330

    add imageView for show dice

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,29 +12,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dice = Dice(6)
-        val luckyNumber = 6
-
-
         val diceRollButton: Button = findViewById(R.id.buttonDice)
-        val txtResultView : TextView = findViewById(R.id.txtResultView)
 
-        diceRollButton.setOnClickListener {
-            val rollResult = dice.roll()
+        diceRollButton.setOnClickListener { rollDice() }
 
-            Log.w("MainActivity", "Your ${dice.slices} sided dice rolled  ${rollResult}!")
-            txtResultView.text = rollResult.toString()
 
-            when(rollResult){
-                6->Toast.makeText(this, "برنده شدی", Toast.LENGTH_LONG).show()
-                5->Toast.makeText(this, "نزدیک بود ولی برنده نشدی!", Toast.LENGTH_SHORT).show()
-                4->Toast.makeText(this, "تو می‌تونی موفق بشی، دوباره تلاش کن", Toast.LENGTH_SHORT).show()
-                3->Toast.makeText(this, "تلاش بیشتر شانس بیشتر!", Toast.LENGTH_SHORT).show()
-                2->Toast.makeText(this, "نشد که بشه! دوباره امتحان کن", Toast.LENGTH_SHORT).show()
-                1->Toast.makeText(this, "خیلی بد شانسی می‌خوای دوباره شانستو امحتحان کنی؟", Toast.LENGTH_SHORT).show()
-            }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val txtResultView: TextView = findViewById(R.id.txtResultView)
+        val imgRollDice: ImageView = findViewById(R.id.imgDice)
+        val rollResult = dice.roll()
+        Log.w("MainActivity", "Your ${dice.slices} sided dice rolled  ${rollResult}!")
+        txtResultView.text = rollResult.toString()
+
+
+        when (rollResult) {
+            6 -> Toast.makeText(this, "برنده شدی", Toast.LENGTH_LONG).show()
+            5 -> Toast.makeText(this, "نزدیک بود ولی برنده نشدی!", Toast.LENGTH_SHORT).show()
+            4 -> Toast.makeText(this, "تو می‌تونی موفق بشی، دوباره تلاش کن", Toast.LENGTH_SHORT)
+                .show()
+            3 -> Toast.makeText(this, "تلاش بیشتر شانس بیشتر!", Toast.LENGTH_SHORT).show()
+            2 -> Toast.makeText(this, "نشد که بشه! دوباره امتحان کن", Toast.LENGTH_SHORT).show()
+            1 -> Toast.makeText(
+                this,
+                "خیلی بد شانسی می‌خوای دوباره شانستو امحتحان کنی؟",
+                Toast.LENGTH_SHORT
+            ).show()
         }
-
+        when (rollResult) {
+            1 -> imgRollDice.setImageResource(R.drawable.dice_1)
+            2 -> imgRollDice.setImageResource(R.drawable.dice_2)
+            3 -> imgRollDice.setImageResource(R.drawable.dice_3)
+            4 -> imgRollDice.setImageResource(R.drawable.dice_4)
+            5 -> imgRollDice.setImageResource(R.drawable.dice_5)
+            6 -> imgRollDice.setImageResource(R.drawable.dice_6)
+        }
 
     }
 }
