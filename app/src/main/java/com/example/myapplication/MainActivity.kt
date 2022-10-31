667e41314ea1d5990250e1f9b0e49cf468dabb08
Shahab Saalami <Shab.salami@gmail.com>
2022-10-31 12:23:27 +0330

    create data classes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView :TextView = findViewById(R.id.textView)
 
+        val dataSource =DataSource()
+        val listOfData = dataSource.loadAffirmations()
+
+       textView .text =  listOfData.size.toString()
     }
 
 }
---
+++ app/src/main/java/com/example/myapplication/model/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.model
+
+data class Affirmation(val stringResourceId: Int) {
+
+}
