7af1b4c31d43d03134dfe0f8020ebbdfc37ee39c
Shahab Saalami <Shab.salami@gmail.com>
2022-11-02 20:31:05 +0330

    work with collections

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+//        first()
+//        second()
+
+//        val triple: (Int) -> Int = { a: Int -> a * 3 }
+        val triple: (Int) -> Int = { it * 3  }
+        println(triple(5))
+    }
+
+    private fun second() {
+        val peopleAges = mutableMapOf<String, Int>(
+            "Shahab" to 50,
+            "Khayam" to 70,
+            "Ferdowsi" to 20
+        )
+
+
+        peopleAges.put("Barbara", 42)
+        peopleAges["Joe"] = 51
+        peopleAges["Fred"] = 31
+
+        println(peopleAges)
+//        peopleAges.forEach { print("key=${it.key} is ${it.value}, ") }
+//        println(peopleAges.map { "${it.key} is ${it.value}" }.joinToString(", ") )
+
+        val filteredNames = peopleAges.filter { it.key.length < 4 }
+        println(filteredNames)
+
+    }
+
+    private fun first() {
+        val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
+        println("list:   ${numbers}")
+        println("sort:   ${numbers.sorted()}")
+        val setOfNumbers = numbers.sorted().toSet()
+        println("setlist:   ${setOfNumbers}")
+
+        //        val mouth: Set<Int> = setOf<Int>(1, 1, 1)
+        val set1 = setOf(1, 2, 3)
+        val set2 = mutableSetOf(3, 2, 1)
+
+        println("$set1 == $set2: ${set1 == set2}")
+        println("contains 7: ${setOfNumbers.contains(7)}")
+
+
     }
 
 }
