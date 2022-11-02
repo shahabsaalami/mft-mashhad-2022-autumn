b6ecfa4be9f093b874f9f25bc32711c2e1704f67
Shahab Saalami <Shab.salami@gmail.com>
2022-11-02 19:11:14 +0330

    clean code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,12 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.adapter.ItemAdapter
-import com.example.myapplication.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-
-        val dataSource =DataSource()
-        val listOfData = dataSource.loadAffirmations()
-
-        recyclerView.adapter= ItemAdapter(this,listOfData)
-        recyclerView.setHasFixedSize(true)
 
 
     }
--- app/src/main/java/com/example/myapplication/model/Affirmation.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Affirmation(@StringRes val stringResourceId: Int,
-                       @DrawableRes val imageResourceId: Int) {
-
-}
