ea7ee2b5d9e04ed26622cafe9f86cc6e2f017ed8
Shahab Saalami <Shab.salami@gmail.com>
2022-11-23 21:31:12 +0330

    init commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,102 +13,14 @@ import java.time.format.DateTimeFormatter
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val TAG = "Coroutine"
+        const val TAG = "MainActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        main()
-//         threadLearning()
-//        coroutines()
-//        runBlock()
-//        coroutineLearning()
-
-    }
-
-    private fun main() {
-        val thread = Thread {
-            Log.i(TAG, "${Thread.currentThread()} has run")
+        GlobalScope.launch {
+            AppDatabase.getDatabase(applicationContext).californiaParkDao().getAll()
         }
-        thread.start()
-    }
-
- 
-    private fun coroutineLearning() {
-        val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
-
-        repeat(3) {
-            GlobalScope.launch {
-                Log.i(TAG, "${Thread.currentThread()} has started")
-                for (i in states) {
-                    Log.d(TAG, "${Thread.currentThread()} - $i")
-                    Thread.sleep(50)
-                }
-            }
-        }
-
-    }
-
-    private fun runBlock() {
-
-        runBlocking {
-            Log.d(TAG, "runBlock: 1")
-            val num1 = async { getValue() }
-            val num2 = async { getValue() }
-            Log.d(TAG, "runBlock: 2")
-            Log.w(TAG, "3 result of num1 + num2 is ${num1.await() + num2.await()}")
-            Log.i(TAG, "4 runBlock: ")
-        }
-    }
-
-    private fun threadLearning() {
-        val thread = Thread {
-            //the thread's name, priority, and thread group
-            //Thread[Thread-4,5,main] has run
-            Log.w(TAG, "${Thread.currentThread()} has run ")
-        }
-        thread.start()
-        val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
-        repeat(3) {
-            Thread {
-                Log.i(TAG, "${Thread.currentThread()} has started")
-                for (i in states) {
-                    Log.d(TAG, "${Thread.currentThread()} - $i")
-                    Thread.sleep(50)
-                }
-            }.start()
-        }
-
-        var count = 0
-        for (i in 1..50) {
-
-            Thread() {
-                count += 1
-                Log.i(TAG, "Thread: i=$i count: $count")
-            }.start()
-        }
-
-    }
-
-    private fun coroutines() {
-
-        repeat(10) {
-            val job: Job = GlobalScope.launch {
-                Log.d(TAG, "coroutines: ${Thread.currentThread()}")
-            }
-        }
-
-    }
-
-    val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_TIME
-    val time = { formatter.format(LocalDateTime.now()) }
-
-    suspend fun getValue(): Double {
-        Log.i(TAG, "entering getValue at ${time()} ")
-        delay(3000)
-        Log.i(TAG, "leaving getValue() at ${time()}")
-        return Math.random()
     }
 }
